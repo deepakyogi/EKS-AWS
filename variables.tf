c10379b23c5de183effe316c61dea7e99df43fdb
deepak <deepak_y@fev.com>
2023-09-12 16:19:20 +0530

    123

---
+++ variables.tf
@@ -0,0 +1,19 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "ap-south-1"
+}
+
+# variable "access_key" {
+#   description = "Access Key"
+#   type        = string
+# }
+
+# variable "secret_key" {
+#   description = "Secret Key"
+#   type        = string
+# }
+
